c7a3183f65be283f321147c5fcaf360c56de1ea9
Denis Snitko <denissnitko@gmail.com>
2020-10-21 22:31:33 +0300

    Update

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,9 @@
+const swiperParams = {
+  slidesPerView: 3,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+}
 
-
+let swiper = new Swiper('.swiper-container', swiperParams);
